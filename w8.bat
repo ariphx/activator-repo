c6842d7b9cf9c1501e90bfbe20559c5c8cc8758d
A R  I P <0.tiny.people@gmail.com>
2025-01-04 10:23:05 +0700

    Update w8.bat

--- w8.bat
+++ w8.bat
@@ -7,8 +7,8 @@ if %i%==2 set KMS_Sev=kms8.MSGuides.com
 if %i%==3 set KMS_Sev=kms9.MSGuides.com
 if %i%==4 goto notsupported
 cscript //nologo slmgr.vbs /skms %KMS_Sev% >nul&echo ============================================================================&echo.&echo.
-cscript //nologo slmgr.vbs /ato | find /i "successfully" && (echo.&echo ============================================================================&echo.&echo #My official blog: MSGuides.com&echo.&echo #How it works: bit.ly/kms-server&echo.&echo #Please feel free to contact me at msguides.com@gmail.com if you have any questions or concerns.&echo.&echo #Please consider supporting this project: donate.msguides.com&echo #Your support is helping me keep my servers running everyday!&echo.&echo ============================================================================&choice /n /c YN /m "Would you like to visit my blog [Y,N]?" & if errorlevel 2 exit) || (echo The connection to my KMS server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto server)
-explorer "http://MSGuides.com"&goto halt
+cscript //nologo slmgr.vbs /ato | find /i "successfully" && (echo.&echo ============================================================================&echo.&echo #Please supporting this project: https://trakteer.id/ariphx&echo.&echo ============================================================================&choice /n /c YN /m "do you want to donate? [Y,N]?" & if errorlevel 2 exit) || (echo The connection to my KMS server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto server)
+explorer "https://trakteer.id/ariphx"&goto halt
 :notsupported
 echo ============================================================================&echo.&echo Sorry! Your version is not supported.
 :halt
