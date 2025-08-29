542e1d51fd7db1cb42071be098262cfb258e9311
A R I P <0.tiny.people@gmail.com>
2025-08-29 19:30:08 +0700

    Update ao2021.cmd

--- ao2021.cmd
+++ ao2021.cmd
@@ -1,24 +1,130 @@
 @echo off
-title Activate Microsoft Office 2021 Without Software - Ariphx&cls&(net session >nul 2>&1)
-if %errorLevel% GTR 0 (echo.&echo Activation cannot proceed. Please Run CMD as administrator...&echo.&goto halt)
-echo =====================================================================================&echo #Project: Activate Microsoft Office 2021 Without Software&echo =====================================================================================&echo.&echo #Supported products:&echo - Microsoft Office Standard 2021&echo - Microsoft Office Professional Plus 2021&echo.&echo.&echo Starting the activation process, please wait...&echo.&(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")&(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")&tasklist /FI "IMAGENAME eq WINWORD.EXE" 2>NUL | find /I /N "WINWORD.EXE">NUL && taskkill /F /IM WINWORD.EXE >nul&tasklist /FI "IMAGENAME eq EXCEL.EXE" 2>NUL | find /I /N "EXCEL.EXE">NUL && taskkill /F /IM EXCEL.EXE >nul&tasklist /FI "IMAGENAME eq POWERPNT.EXE" 2>NUL | find /I /N "POWERPNT.EXE">NUL && taskkill /F /IM POWERPNT.EXE >nul&(REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Common\Licensing" /f >nul 2>NUL)&(cscript //nologo ospp.vbs /setprt:1688 >nul || goto wshdisabled)&(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)&cscript //nologo ospp.vbs /unpkey:WFG99 >nul&cscript //nologo ospp.vbs /unpkey:6MWKP >nul&cscript //nologo ospp.vbs /unpkey:6F7TH >nul
+title Activate Microsoft Office 2021 Without Software - Ariphx
+cls
+setlocal ENABLEDELAYEDEXPANSION
+
+echo.
+echo ================================================
+echo    Microsoft Office 2021 Activation Script
+echo ================================================
+echo.
+echo    - Microsoft Office Standard 2021
+echo    - Microsoft Office Professional Plus 2021
+echo.
+echo.
+echo =====================================================================================
+echo    [PROSES] Mengaktivasi Office 2021 Anda, silahkan tunggu...
+echo =====================================================================================
+echo.
+
+:: Periksa hak akses administrator
+net session >nul 2>&1
+if !errorLevel! GTR 0 (
+    echo.
+    echo [GALAT] Aktivasi tidak bisa dilanjutkan.
+    echo Skrip ini harus dijalankan sebagai administrator.
+    echo Silakan klik kanan pada file .cmd dan pilih "Run as administrator".
+    echo.
+    goto halt
+)
+
+:: Temukan folder instalasi Office
+if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" (
+    cd /d "%ProgramFiles%\Microsoft Office\Office16"
+) else if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" (
+    cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16"
+) else (
+    goto notsupported
+)
+
+:: Tutup aplikasi Office yang sedang berjalan
+tasklist /FI "IMAGENAME eq WINWORD.EXE" 2>NUL | find /I /N "WINWORD.EXE">NUL && taskkill /F /IM WINWORD.EXE >nul
+tasklist /FI "IMAGENAME eq EXCEL.EXE" 2>NUL | find /I /N "EXCEL.EXE">NUL && taskkill /F /IM EXCEL.EXE >nul
+tasklist /FI "IMAGENAME eq POWERPNT.EXE" 2>NUL | find /I /N "POWERPNT.EXE">NUL && taskkill /F /IM POWERPNT.EXE >nul
+
+:: Hapus lisensi lama dan set port KMS
+(REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Common\Licensing" /f >nul 2>NUL)
+(cscript //nologo ospp.vbs /setprt:1688 >nul || goto wshdisabled)
+
+:: Pasang lisensi KMS dan hapus kunci produk yang mungkin bertentangan
+(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
+cscript //nologo ospp.vbs /unpkey:WFG99 >nul
+cscript //nologo ospp.vbs /unpkey:6MWKP >nul
+cscript //nologo ospp.vbs /unpkey:6F7TH >nul
+
 :ucpkey
 cscript //nologo ospp.vbs /dstatus | find /i "Last 5 characters of installed product key" > temp.txt || goto skunpkey
 set /P bpkey= < temp.txt
 set pkeyt=%bpkey:~-5%
-echo.&cscript //nologo ospp.vbs /unpkey:%pkeyt% >nul&goto ucpkey
+cscript //nologo ospp.vbs /unpkey:%pkeyt% >nul
+goto ucpkey
+
 :skunpkey
-echo.&echo =====================================================================================&echo Activating your Office 2021, please wait...&echo =====================================================================================&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo ospp.vbs /setprt:1688 >nul&set i=1&cscript //nologo ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH >nul||goto notsupported
+echo.
+echo =====================================================================================
+echo    [PROSES] Mengaktivasi Office 2021, mohon tunggu...
+echo =====================================================================================
+echo.
+cscript //nologo slmgr.vbs /ckms >nul
+cscript //nologo ospp.vbs /setprt:1688 >nul
+set i=1
+cscript //nologo ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH >nul||goto notsupported
+
 :skms
 if %i% GTR 5 (goto busy) else if %i% LEQ 5 (set KMS=s8.mshost.pro)
 cscript //nologo ospp.vbs /sethst:%KMS% >nul
+
 :ato
-echo.&echo =====================================================================================&cscript //nologo ospp.vbs /act >nul&timeout /t 5 >nul&echo.&cscript //nologo ospp.vbs /act | find /i "successful" && (echo.&echo =====================================================================================&echo.&echo.&echo Support us by donating via trakteer.id/ariphx so this method can continue to be used.&echo.& if errorlevel 2 exit) || (echo It seems to take more time, please wait... & echo. & echo. & set /a i+=1 & timeout /t 10 >nul & goto skms)&timeout /t 7 >nul&explorer "https://trakteer.id/ariphx"&echo.&echo.&goto halt
+echo.
+echo =====================================================================================
+cscript //nologo ospp.vbs /act >nul
+timeout /t 5 >nul
+echo.
+cscript //nologo ospp.vbs /act | find /i "successful" && (
+    echo.
+    echo =====================================================================================
+    echo.
+    echo [BERHASIL] Produk Anda berhasil diaktivasi!
+    echo.
+    echo Support us by donating via trakteer.id/ariphx so this method can continue to be used.
+    echo.
+    timeout /t 7 >nul
+    explorer "https://trakteer.id/ariphx"
+    echo.
+    echo.
+    goto halt
+) || (
+    echo Sepertinya butuh waktu lebih, silahkan tunggu...
+    echo.
+    echo.
+    set /a i+=1
+    timeout /t 10 >nul
+    goto skms
+)
+
 :notsupported
-echo =====================================================================================&echo.&echo Sorry, your Office 2021 is not supported.&echo.&goto halt
+echo =====================================================================================
+echo.
+echo [GALAT] Maaf, Office 2021 Anda tidak didukung.
+echo.
+goto halt
+
 :wshdisabled
-echo =====================================================================================&echo.&echo Sorry, activation failed because Windows Script Host access is disabled.&echo.&echo The solution is to follow the guide in the video tutorial that opens to enable Windows Script.&echo.&timeout /t 7 >nul&explorer "https://youtu.be/vqGMSQnWMIY"&goto halt
+echo =====================================================================================
+echo.
+echo [GALAT] Maaf, aktivasi gagal karena Windows Script Host dinonaktifkan.
+echo.
+goto halt
+
 :busy
-echo =====================================================================================&echo.&echo.&echo Sorry, activation failed due to an unstable internet connection.&echo.&echo Please connect your laptop to a different WiFi network and try again.&echo.&goto halt
+echo =====================================================================================
+echo.
+echo [GALAT] Maaf, aktivasi gagal karena koneksi internet Anda tidak stabil.
+echo.
+goto halt
+
 :halt
-cd %~dp0&del %0 >nul&pause >nul
+cd %~dp0
+del temp.txt >nul 2>nul
+del %0 >nul 2>nul
+pause >nul
